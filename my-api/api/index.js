cae364dc527c0056a0c91d8a6ea9bbed1ff5cf9d
verybadboyoldteam-rgb <verybadboyoldteam@gmail.com>
2026-07-29 23:19:51 +0300

    Add files via upload
    First commit

---
+++ my-api/api/index.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+  const data = {
+    name: "Алексей",
+    age: 35,
+    profession: "Руководитель отдела мобильной разработки",
+    hobbies: [
+      "менять скины в Quake 3",
+      "читать стихи Есенина",
+      "тащиться от Тринити из Матрицы"
+    ],
+    location: {
+      currentCity: "Минск",
+      currentCountry: "Беларусь",
+      dreamCountry: "Франция"
+    }
+  };
+
+  res.status(200).json(data);
+}
