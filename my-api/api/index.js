413f02d05a9884b7aa00c51d193aa2994d0ecb47
verybadboyoldteam-rgb <verybadboyoldteam@gmail.com>
2026-07-29 23:44:18 +0300

    Update index.js
    fix api

--- my-api/api/index.js
+++ my-api/api/index.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   const data = {
     name: "Алексей",
     age: 35,
@@ -16,4 +16,4 @@ export default function handler(req, res) {
   };
 
   res.status(200).json(data);
-}
+};
